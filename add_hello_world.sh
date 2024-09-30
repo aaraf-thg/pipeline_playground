7c2644e6132f78d0a6b67275b2bc4aee35430baa
Aaraf <Aaraf.Hossain44@thg.com>
2024-09-30 15:15:57 +0100

    added time date to add_hello_world.sh

--- add_hello_world.sh
+++ add_hello_world.sh
@@ -5,12 +5,14 @@ PYTHON_FILE="helloworld.py"
 
 # Append another print statement to the Python script
 echo 'print("hello world")' >> $PYTHON_FILE
+# Get the current date and time
+CURRENT_DATETIME=$(date "+%Y-%m-%d %H:%M:%S")
 
 # Add the changes to Git
 git add $PYTHON_FILE
 
-# Commit the changes with a message
-git commit -m "Added another hello world print statement"
+# Commit the changes with a message that includes the current date and time
+git commit -m "Added another hello world print statement on $CURRENT_DATETIME"
 
 # Push the changes to the remote repository
 git push origin main
